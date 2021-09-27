270911a34ed61e4e519ba4a5c0ce64023067e502
MASTRIO <mastrio5772@gmail.com>
2021-09-28 05:22:40 +1000

    cuttin down

--- door/src/main.rs
+++ door/src/main.rs
@@ -1,9 +1,31 @@
 use bevy::prelude::*;
 use bevy::window::*;
 
+struct TaskBar;
+
+struct Colours {
+    taskbar: Handle<ColorMaterial>,
+}
+
+fn spawn_taskbar(mut commands: Commands, colours: Res<Colours>) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: colours.taskbar.clone(),
+            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+            ..Default::default()
+        })
+        .insert(TaskBar);
+}
+
 // Contains code that needs to run when the OS is started
-fn setup() {
-    
+fn setup(mut commands: Commands, mut colours: ResMut<Assets<ColorMaterial>>) {
+    // Creates the camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    // Setup Colours
+    commands.insert_resource(Colours {
+        taskbar: colours.add(Color::rgb(0.7, 0.7, 0.7).into()),
+    });
 }
 
 // Contains code that needs to be run every tick
@@ -22,6 +44,7 @@ fn main() {
             ..Default::default()
         })
         .add_startup_system(setup.system())
+        .add_startup_stage("os_main", SystemStage::single(spawn_taskbar.system()))
         .add_system(process_loop.system())
         .add_plugins(DefaultPlugins)
         .run();
